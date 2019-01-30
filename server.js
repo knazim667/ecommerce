14e0d9b0225238aefe7cf6685d7c5d541f248ca0
Muhammad Nazam <muhammadnazam@Muhammads-MBP.attlocal.net>
2019-01-29 23:36:05 -0800

    add morgan

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+
+
+
+
 
 const app = express();
+
+//Middlware
+app.use(morgan('dev'));
 
 
 app.get('/', (req, res) => {
 
     const name = "Nazam";
     res.json("My Name is " + name);
-})
+});
+
+app.get('/name', (req, res) => {
+    res.json("My Name is Nazam");
+});
 
 app.listen(3000, function(err) {
     if(err)
