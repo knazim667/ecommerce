87cde49ecd52145c66fa07622d5350bbaeba834f
Muhammad Nazam <muhammadnazam@Muhammads-MBP.attlocal.net>
2019-01-30 01:09:50 -0800

    adding user schema and hashing password

---
+++ models/user.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+
+const Schema = mongoose.Schema;
+
+/* User Schema */
+const UserSchema = new Schema ({
+    email:{
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    passowrd:{
+        type: String
+    },
+
+    profile: {
+        name : {
+            type:String,
+            default : ''
+        },
+        picture: {
+            type: String,
+            default : ''
+        }
+    },
+
+    address: String,
+    history: [{
+        date: Date,
+        paid: {
+            type: Number,
+            default: 0
+        },
+    }]
+});
+
+/* Hash Password */
+
+UserSchema.pre('save', function(next){
+    const user = this;
+
+    if(!user.isModified('password')) return next();
+    bcrypt.genSalt(10, function(err, salt) {
+        if(err) return next(err);
+        bcrypt.hash(user.passowrd, salt, null, function(err, hash){
+            if(err) return next(err);
+            user.passowrd = hash;
+            next();
+        });
+    });
+});
+
+/* compare password */
+
+UserSchema.methods.comparePassword = function(passowrd){
+    return bcrypt.compareSync(passowrd, this.passowrd);
+}
